f45e8a9240a77babbdff9d763c330415167336ad
Irgey <116591589+Irgey@users.noreply.github.com>
2023-06-22 15:54:26 +0200

    auth fix

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -10,12 +10,14 @@ const authenticate = async (req, res, next) => {
 
   const [bearer, token] = authorization.split(" ");
   if (bearer !== "Bearer") {
+    console.log("bearer error");
     next(HttpError(401));
   }
   try {
     const { id } = jwt.verify(token, SECRET_TOKEN_KEY);
     const user = await User.findById(id);
-    if (!user || !user.token || user.token === token) {
+    if (!user || !user.token) {
+      console.log("user error");
       next(HttpError(401));
     }
     req.user = user;
