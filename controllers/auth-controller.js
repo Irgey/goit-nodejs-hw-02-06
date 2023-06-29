6b7a30d70389c4b15dcf359be4d98c1fb4cc968b
Irgey <116591589+Irgey@users.noreply.github.com>
2023-06-29 16:07:50 +0200

    hw05 complete

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,28 +1,35 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../models/user");
-
+const fs = require("fs/promises");
+const path = require("path");
 const { ctrlWrapper } = require("../decorators");
-
+const gravatar = require("gravatar");
 const { HttpError } = require("../helpers");
+const Jimp = require("jimp");
 
 const { SECRET_TOKEN_KEY } = process.env;
 
+const avatarsDir = path.resolve("public", "avatars");
 const register = async (req, res) => {
   const { email, password } = req.body;
+  const avatarURL = gravatar.url(email, { size: "256" });
+
   const user = await User.findOne({ email });
   if (user) {
     throw HttpError(409, "Email in use");
   }
 
   const hashPassword = await bcrypt.hash(password, 10);
-  const newUser = await User.create({ ...req.body, password: hashPassword });
+  const newUser = await User.create({
+    ...req.body,
+    password: hashPassword,
+    avatarURL,
+  });
 
-  res
-    .status(201)
-    .json({
-      user: { email: newUser.email, subscription: newUser.subscription },
-    });
+  res.status(201).json({
+    user: { email: newUser.email, subscription: newUser.subscription },
+  });
 };
 
 const login = async (req, res) => {
@@ -48,9 +55,9 @@ const login = async (req, res) => {
 };
 
 const getCurrent = (req, res) => {
-  const { email, subscription } = req.user;
+  const { email, subscription, avatarURL } = req.user;
 
-  res.json({ email, subscription });
+  res.json({ email, subscription, avatarURL });
 };
 
 const logout = async (req, res) => {
@@ -64,10 +71,30 @@ const updateSubscription = async (req, res) => {
   await User.findByIdAndUpdate(_id, { subscription });
   res.status(200).json({ message: "Subscription successfully updated" });
 };
+
+const updateAvatar = async (req, res) => {
+  const { _id } = req.body;
+  console.log(req.body, req.file);
+  const { path: oldPath, filename } = req.file;
+  const newPath = path.join(avatarsDir, filename);
+  await Jimp.read(oldPath)
+    .then((avatar) => {
+      return avatar.resize(250, 250).write(oldPath);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+  await fs.rename(oldPath, newPath);
+  const avatarURL = path.join("avatars", filename);
+
+  await User.findByIdAndUpdate(_id, { avatarURL });
+  res.status(200).json({ avatarURL });
+};
 module.exports = {
   register: ctrlWrapper(register),
   login: ctrlWrapper(login),
   getCurrent: ctrlWrapper(getCurrent),
   logout: ctrlWrapper(logout),
   updateSubscription: ctrlWrapper(updateSubscription),
+  updateAvatar: ctrlWrapper(updateAvatar),
 };
--- middlewares/index.js
+++ middlewares/index.js
@@ -2,9 +2,11 @@ const isBodyEmpty = require("./isBodyEmpty");
 const isValidId = require("./isValidId");
 const isFavoriteExists = require("./isFavoriteExists");
 const authenticate = require("./authenticate");
+const upload = require("./upload");
 module.exports = {
   isBodyEmpty,
   isValidId,
   isFavoriteExists,
   authenticate,
+  upload,
 };
